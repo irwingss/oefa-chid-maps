55da393a42e8921a32e20e89d377d35f0a61da98
Irwing S. Saldaña <irwingssaldu@gmail.com>
2025-07-07 19:31:37 -0500

    intento 3

--- Unir Capa Lotes y Emergencias.R
+++ Unir Capa Lotes y Emergencias.R
@@ -3,11 +3,10 @@ library(dplyr)
 library(openxlsx)
 library(sf)
 
-# 1. Leer los datos
-lotes <- read_csv("Capa_lotes_WKT.csv")
+# 1. Leer la base de emergencias
 emergencias <- read.xlsx("00_BD EMERGENCIAS-nuevo-formato.xlsx")
 
-# 2. Unificar emergencias en formato WKT tipo POINT
+# 2. Seleccionar y transformar
 puntos <- emergencias %>%
   select(ZONA, Este, Norte, Unidad.Fiscalizable, Administrado) %>%
   rename(
@@ -17,28 +16,11 @@ puntos <- emergencias %>%
   mutate(
     UF_nombre = UNIDAD_FIS,
     geometry_wkt = paste0("POINT (", Este, " ", Norte, ")")
-  )
+  ) %>%
+  filter(!is.na(Este), !is.na(Norte))  # filtrar coordenadas válidas
 
-# 3. Añadir columnas faltantes
-cols_lotes <- colnames(lotes)
-for (col in setdiff(cols_lotes, colnames(puntos))) {
-  puntos[[col]] <- NA
-}
-puntos <- puntos %>% select(all_of(cols_lotes))
+# 3. Convertir a objeto sf
+geo_puntos <- st_as_sf(puntos, wkt = "geometry_wkt", crs = 4326)
 
-# 4. Unir ambos datasets
-unificado <- bind_rows(lotes, puntos)
-
-# 5. Limpiar geometrías inválidas (las que tienen NA, espacios vacíos o errores)
-unificado <- unificado %>%
-  filter(!is.na(geometry_wkt), !grepl("NA", geometry_wkt))
-
-# 6. Convertir a objeto sf
-geo <- st_as_sf(unificado, wkt = "geometry_wkt", crs = 4326)
-
-# 7. Exportar a GeoJSON
-st_write(geo, "Capa_Lotes_y_Emergencias.geojson", driver = "GeoJSON")
-
-# Exportar CSV
-write.csv(geo, "Capa_Lotes_y_Emergencias.csv", row.names = FALSE, 
-          sep = ";", dec = ".")
+# 4. Exportar como GeoJSON
+st_write(geo_puntos, "Emergencias.geojson", driver = "GeoJSON")
